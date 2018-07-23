b13f29a7b150d8ef68f01692d13e3703fb7255e0
Matt Carpenter <mattcarpenter@gmail.com>
2018-07-22 21:36:34 -0700

    furigana

--- server/handlers/api/project/create/index.js
+++ server/handlers/api/project/create/index.js
@@ -1,10 +1,22 @@
 const dao = require('../../../../lib/dao');
 const srt = require('../../../../lib/srt');
+const kuroshiro = require('kuroshiro');
+const wanakana = require('wanakana');
+
+kuroshiro.init();
 
 module.exports = function (req, h) {
   return new Promise((resolve, reject) => {
 
     const captions = srt.parse(req.payload.englishSrt);
+    captions.forEach((caption) => {
+      const input = caption.text;
+      caption.text = kuroshiro.convert(input, {
+        mode: 'furigana'
+      });
+      caption.tokens = wanakana.tokenize(input);
+    });
+
     resolve(JSON.stringify(captions));
 
     return;
